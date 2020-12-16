22bbe89c477bb4fbfb9d10b547faf8697fce0e69
Nik <git@vaessen.tech>
2020-12-16 08:24:54 +0100

    experiment with SLURM

---
+++ experiments/example/example.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --partition=csedu
+#SBATCH --account=nvaessen
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=00:30:00
+#SBATCH --gpus 1
+
+DISK=/scratch-csedu/other/nik/
+DATA_PATH=$DISK/data/voxceleb/
+SAVE_PATH=$DISK/experiments/speaker/voxceleb/example
+
+srun python trainSpeakerNet.py \
+--model ResNetSE34L \
+--trainfunc angleproto \
+--batch_size 400 \
+--nPerSpeaker 2 \
+--train_list $DATA_PATH/train_list.txt \
+--train_path $DATA_PATH/voxceleb2/ \
+--test_list $DATA_PATH/test_list.txt \
+--test_path $DATA_PATH/voxceleb1/ \
+--save_path $SAVE_PATH
